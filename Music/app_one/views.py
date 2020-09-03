d2357e1045edf5f72ebac4e4a9310940bf9edfbb
cduboff <clbduboff@gmail.com>
2020-09-03 12:48:05 -0400

    fixing files

--- Music/app_one/views.py
+++ Music/app_one/views.py
@@ -7,7 +7,4 @@ def index(request):
 def search(request):
     search = request.POST('song')
     search.replace(" ", "%20")
-    request.GET('https://api.spotify.com/v1/search?q=tania%20bowra&type=artist&Authorization:%20Bearer:%205d76ac0e1b4c4d75b7627674b8129495')
-    return render(request, 'music.html')
-    # GET https://api.spotify.com/v1/search
-    # "https://api.spotify.com/v1/search?q=tania%20bowra&type=artist" -H "Authorization: Bearer 5d76ac0e1b4c4d75b7627674b8129495"
+    
